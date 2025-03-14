05486998b9e09ef3ca39905cba7e5c6e5b0617b9
Bibek Shah <124855267+Bibekshah123@users.noreply.github.com>
2025-03-14 20:33:01 +0545

    .

--- first_project/members/views.py
+++ first_project/members/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Members
+
 
 def members(request):
-  template = loader.get_template('first.html')
-  return HttpResponse(template.render())
+  mymembers = Members.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
