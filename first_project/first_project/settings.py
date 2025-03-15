1c1dbfe07f8002af5f32bb83a2b4444555719169
Bibek Shah <124855267+Bibekshah123@users.noreply.github.com>
2025-03-15 11:04:28 +0545

    .

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c_223ackhmj%5*-0=di1o7(*fm*ylyk)a&uj#o@qo08dbe)bt+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
